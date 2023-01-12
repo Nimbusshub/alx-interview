1246f2a51361f1fae05a70b969adcd9bc38b23e9
Nimah <busayonimota1603@gmail.com>
2023-01-12 21:38:34 +0100

    [Feats]: Determines the minimum operations on a file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,3 +3,31 @@
 """ Function to determine the minimum number of operations to
 result in exactly n H characters of a file
 """
+
+
+def minOperations(n: int):
+    """Calculates the minumum number of operations
+    Args:
+        n (int): number of H characters in a file
+    Returns:
+        no_ops (int): number of operations
+    """
+    if (n < 1):
+        return 0
+
+    start = 1
+    count = 0
+    no_ops = 0
+
+    while start < n:
+        rem = n - start
+        if (rem % start == 0):
+            count = start
+            start += count
+            no_ops += 2
+
+        else:
+            start += count
+            no_ops += 1
+
+    return no_ops
